c92a8600464a68b96cd22f50b6cd05e8e103034f
vmadmin <vmadmin@webvm-wan.localdomain>
2022-02-10 12:04:12 +0100

    Uebung1-3 komplett, deletion

--- Uebungen/Uebung2-3/delete.php
+++ Uebungen/Uebung2-3/delete.php
@@ -1,43 +1,40 @@
 <?php
-    $servername = "localhost";
-    $username = "vmadmin";
-    $password = "sml12345";
-    $database = "northwind";
-    $options = [
-        PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ];
-    $conn = null;
+$servername = "localhost";
+$username = "vmadmin";
+$password = "sml12345";
+$database = "northwind";
+$options = [
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+$conn = null;
 
-    try 
-    {
-        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password, $options);
-    }
-    catch(PDOException $e)
-    {
-        echo "Connection failed: " . $e->getMessage();
-        die();
-    }
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password, $options);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die;
+}
 
-    $query = $conn->prepare("DELETE o, od, i FROM orders AS o 
-        JOIN order_details AS od ON o.id = od.order_id 
-        -- INNER JOIN inventory_transactions AS it ON o.id = it.customer_order_id
-        JOIN invoices AS i ON o.id = i.order_id
-        WHERE id = :order_id");
+$id = $_GET['id'];
+if (!$id) {
+    die;
+}
 
-    echo "<pre>";
-    var_dump($query);
+$params = [
+    ':id' => $id
+];
 
-    if($query->execute([':order_id' => $_GET['id']]))
-    {
-        echo "deletion complete";
-    }
-    else
-    {
-        echo "SQL Error <br />";
-        echo $statement->queryString."<br />";
-        echo $statement->errorInfo()[2];
-    }
+$statement = $conn->prepare("SELECT customer_id FROM orders WHERE orders.id = :id;");
+$statement->execute($params);
+$customerId = $statement->fetch()['customer_id'];
 
-?>
+$statement = $conn->prepare("DELETE FROM order_details WHERE order_details.order_id = :id;");
+$statement->execute($params);
+$statement = $conn->prepare("DELETE FROM invoices WHERE invoices.order_id = :id;");
+$statement->execute($params);
+$statement = $conn->prepare("DELETE FROM orders WHERE orders.id = :id;");
+$statement->execute($params);
+
+header("Location: http://127.0.0.1:8000/bestellungen.php?id=$customerId");
